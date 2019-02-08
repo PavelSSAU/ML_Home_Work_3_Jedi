4d4e68d890447ed6a8aad9f4e70ad530147e26f6
pavel.ssau@gmail.com <pavel.ssau@gmail.com>
2019-02-08 15:53:36 +0400

    V2 ДЗ_3

--- PythonApplication1/Jedi.py
+++ PythonApplication1/Jedi.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np  
 import matplotlib.pyplot as plt
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-
 #данные о производстве вина в Австралии за несколько лет
 dataset = pd.read_csv("wine_Australia.tsv", sep = '\t') #параметр sep задает разделитель данных. Формат tsv - разделителем является табуляция
 print(dataset.head())
@@ -13,15 +12,20 @@ time_series = dataset['spark'].values
 plt.plot(time_series, color = 'blue')
 
 #задаем вектора параметров (см. лекции). Поиграйте с этими параметрами, чтобы получить хороший результат (тут специально все плохо)
-pdq = (2, 1, 1)
-PDQS = (1, 1, 1, 10)
-model = SARIMAX(time_series, order = pdq, seasonal_order = PDQS)
+pdq1 = (1, 1, 1)
+PDQS1 = (1, 1, 1, 12)
+pdq2 = (1, 1, 3)
+PDQS2 = (1, 1, 1, 12)
+model1 = SARIMAX(time_series, order = pdq1, seasonal_order = PDQS1)
+model2 = SARIMAX(time_series, order = pdq2, seasonal_order = PDQS2)
 
-#тренируем модель и делаем предсказание
-model_fit = model.fit()
-predicted = model_fit.predict(start=1, end=len(time_series) + 10) #метод predict принимет два параметра - когда начать предсказывать и когда пора остановиться. Здесь мы делаем предсказание на 10 месяцев вперед
-
+#тренируем модель и дела10м предсказание
+model_fit1 = model1.fit()
+predicted1 = model_fit1.predict(start=1, end=len(time_series) + 8) 
+model_fit2 = model2.fit()
+predicted2 = model_fit2.predict(start=1, end=len(time_series) + 8)
 #смотрим на наше предсказание
-plt.plot(predicted, color = 'red')
+plt.plot(predicted1, color = 'red')
+plt.plot(predicted2, color = 'green')
 plt.show()
 
